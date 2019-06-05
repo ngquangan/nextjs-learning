be94669b3c1d136c5e3d4776ccebea2b0c82ff75
Nguyen Quang An <ngquangan@gmail.com>
2019-06-05 19:31:17 +0700

    Fetch data from hacker news api

--- hacker-next/pages/index.js
+++ hacker-next/pages/index.js
@@ -1,9 +1,31 @@
-const Index = () => {
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+
+const Index = (props) => {
+  if (props.stories.length === 0) return <Error statusCode = { 503 } />;
   return (
     <div>
-      Clone hacker new
+      <h1>Hacker next</h1>
+      <div>
+        { props.stories.map(story => (
+          <h3 key = { story.id } >{ story.title }</h3>
+        )) }
+      </div>
     </div>
   )
+}
+
+Index.getInitialProps = async () => {
+  let stories = [];
+  try {
+    const res = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+    stories = await res.json();
+  } catch (error) {
+    console.log(error);
+    stories = [];
+  }
+
+  return { stories }
 }
 
 export default Index;
